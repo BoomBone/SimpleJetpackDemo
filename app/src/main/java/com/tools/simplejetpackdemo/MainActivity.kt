e7cc7a728fbf5d38980fe2cbf084fc1284cfc7f4
Ting <ticccccc@163.com>
2018-10-29 16:51:32 +0800

    Add lifecycle dependence,create ViewModel

--- app/src/main/java/com/tools/simplejetpackdemo/MainActivity.kt
+++ app/src/main/java/com/tools/simplejetpackdemo/MainActivity.kt
@@ -3,12 +3,17 @@ package com.tools.simplejetpackdemo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 import com.tools.simplejetpackdemo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var mainViewModel: MainActivityViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        mainViewModel = ViewModelProviders.of(this).get(MainActivityViewModel::class.java)
+
     }
 }
---
+++ app/src/main/java/com/tools/simplejetpackdemo/MainActivityViewModel.kt
@@ -0,0 +1,6 @@
+package com.tools.simplejetpackdemo
+
+import androidx.lifecycle.ViewModel
+
+class MainActivityViewModel : ViewModel() {
+}
